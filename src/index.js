daf725e8e5ca7121ecfc09bf0f94c243ecdf0be2
rafmorei <rafael.mattos09@gmail.com>
2023-10-20 23:55:23 -0300

    bug

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(rotas);
 
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("Server pt 3000");
 });
