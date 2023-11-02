eae2cef6b5df995472f5bc8ba39831981ae80ad4
rafmorei <rafael.mattos09@gmail.com>
2023-11-02 07:25:07 -0300

    cadastro_pedido_v2

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -16,26 +16,11 @@ const cadastrarPedido = async (req, res) => {
       return res.status(404).json(clienteExiste);
     }
 
-    for (j = 0; j < pedido_produtos.length; j++) {
-      const estoque = await knex("produtos")
-        .select("quantidade_estoque", "valor")
-        .where({ id: pedido_produtos[j].produto_id })
-        .first();
-
-      if (estoque.quantidade_estoque < pedido_produtos[j].quantidade_produto) {
-        return res
-          .status(400)
-          .json({ mensagem: "Quantidade em estoque insuficiente" });
-      }
-    }
-
-    for (i = 0; i < pedido_produtos.length; i++) {
-      console.log(pedido_produtos[i].produto_id);
-      console.log(pedido_produtos[i].quantidade_produto);
+    for (const pedidoProduto of pedido_produtos) {
       const produtoExiste = await validaCadastro(
         "produtos",
         "id",
-        pedido_produtos[i].produto_id,
+        pedidoProduto.produto_id,
         "select"
       );
 
@@ -45,41 +30,67 @@ const cadastrarPedido = async (req, res) => {
 
       const estoque = await knex("produtos")
         .select("quantidade_estoque", "valor")
-        .where({ id: pedido_produtos[i].produto_id })
+        .where({ id: pedidoProduto.produto_id })
         .first();
-      console.log(estoque.quantidade_estoque);
 
-      const valorTotalItem =
-        estoque.valor * pedido_produtos[i].quantidade_produto;
-      console.log(valorTotalItem);
-      const pedido = await knex("pedidos")
+      if (estoque.quantidade_estoque < pedidoProduto.quantidade_produto) {
+        return res
+          .status(400)
+          .json({ mensagem: "Quantidade em estoque insuficiente" });
+      }
+    }
+
+    const transaction = await knex.transaction();
+
+    try {
+      let valorTotalPedido = 0;
+
+      const pedido = await transaction("pedidos")
         .insert({
           cliente_id,
           observacao,
-          valor_total: valorTotalItem,
+          valor_total: valorTotalPedido,
         })
-        .returning("*");
+        .returning("id");
 
-      console.log(pedido);
+      for (const pedidoProduto of pedido_produtos) {
+        const produtoInfo = await transaction("produtos")
+          .select("valor")
+          .where({ id: pedidoProduto.produto_id })
+          .first();
 
-      const pedido_produtos_ = await knex("pedido_produtos").insert({
-        pedido_id: pedido[0].id,
-        produto_id: pedido_produtos[i].produto_id,
-        quantidade_produto: pedido_produtos[i].quantidade_produto,
-        valor_produto: estoque.valor,
-      });
-      const valorAtualizadoEstoque =
-        estoque.quantidade_estoque - pedido_produtos[i].quantidade_produto;
-      const atualizaEstoque = await knex("produtos")
-        .where({ id: pedido_produtos[i].produto_id })
-        .update({
-          quantidade_estoque: valorAtualizadoEstoque,
+        const valorTotalItem =
+          produtoInfo.valor * pedidoProduto.quantidade_produto;
+        valorTotalPedido += valorTotalItem;
+
+        await transaction("pedido_produtos").insert({
+          pedido_id: pedido[0].id,
+          produto_id: pedidoProduto.produto_id,
+          quantidade_produto: pedidoProduto.quantidade_produto,
+          valor_produto: produtoInfo.valor,
         });
+
+        await transaction("produtos")
+          .where({ id: pedidoProduto.produto_id })
+          .decrement("quantidade_estoque", pedidoProduto.quantidade_produto);
+      }
+
+      await transaction("pedidos")
+        .where({ id: pedido[0].id })
+        .update({ valor_total: valorTotalPedido });
+
+      await transaction.commit();
+
+      return res
+        .status(201)
+        .json({ mensagem: "Pedido cadastrado com sucesso" });
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
     }
-    return res.status(201).json("all right");
   } catch (error) {
-    console.log(error.message);
-    return res.status(500).json({ mensagem: "Erro interno do servidor!" });
+    console.error(error);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
   }
 };
 
